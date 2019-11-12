25e879a24d374d54f5f5f36da46d8e3af2377018
Jan Mrázek <email@honzamrazek.cz>
2019-11-12 19:54:51 +0100

    Fix windows build

--- buildWin.bat
+++ buildWin.bat
@@ -1,1 +1,1 @@
-pyinstaller -wF -pelegooMarsutility -p%LOCALAPPDATA%\Programs\Python\Python37-32\Lib\site-packages\scipy\.libs elegooMarsUtility\xyCompensateGui.py
+pyinstaller -wF -pelegooMarsutility -p%LOCALAPPDATA%\Programs\Python\Python37-32\Lib\site-packages\scipy\.libs --hidden-import numpy.random.common --hidden-import numpy.random.bounded_integers --hidden-import numpy.random.entropy elegooMarsUtility\xyCompensateGui.py
